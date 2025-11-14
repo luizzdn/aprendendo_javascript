43bb8873d81228f40ddce5767329fea3f4a79832
luizzdn <luizd.martinss@gmail.com>
2025-11-13 21:19:03 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,8 @@
+let pessoa = {
+    nome: "Rafael",
+    idade: 26,
+    peso: 94,
+    altura: 1.81
+}
+
+console.log(`Meu nome é ${pessoa.nome} tenho ${pessoa.idade} anos tenho ${pessoa.peso}kg e tenho ${pessoa.altura} de altura`)
